80d2e3537c29d22153370a476dec8e988f492a92
MarkoDjurdjevic03 <markodjurdjevic03@gmail.com>
2026-07-02 16:17:58 +0200

    baza optimizovano

--- v2/Marko/query3.py
+++ v2/Marko/query3.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 import json
 
 client = MongoClient("mongodb://localhost:27017/")
-db = client["projectdb"]
+db = client["optimizovano"]
 collection = db["products"]
 
 collection.create_index([
